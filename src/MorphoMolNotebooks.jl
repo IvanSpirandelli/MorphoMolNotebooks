bc489da7b75f53d40ef46d9112dd52d1406147d5
Ivan Spirandelli <ivan@spirandelli.de>
2024-10-30 12:00:32 -0700

    visualization update

--- src/MorphoMolNotebooks.jl
+++ src/MorphoMolNotebooks.jl
@@ -3,7 +3,7 @@ module MorphoMolNotebooks
 using GLMakie
 using MorphoMol
 
-export visualize_persistence, visualize_persistence_and_ma
+export visualize_persistence_with_diagrams, visualize_persistence_and_ma_with_diagrams, visualize_ma_and_persistence
 include("visualization.jl")
 
 end #module MorphoMolNotebooks
--- src/visualization.jl
+++ src/visualization.jl
@@ -1,4 +1,4 @@
-function visualize_persistence(input, output)
+function visualize_persistence_with_diagrams(input, output)
     realizations = [[e for e in eachcol(hcat(MorphoMol.Utilities.get_matrix_realization(state, input["template_centers"])...))] for state in output["states"]]
     realizations = [hcat(realizations[i]...) for i in 1:length(realizations)]
     dgms = output["PDGMs"]
@@ -89,18 +89,10 @@ function visualize_persistence(input, output)
 
     scatter!(conf_ax, points, markersize = conf_ms, color = colors, colormap = :rainbow)
 
-    # for (label, layout) in zip(["A", "B", "C", "D"], [cg, pg, mg, eg])
-    #     Label(layout[1, 1, TopLeft()], label,
-    #         fontsize = 12,
-    #         font = :bold,
-    #         padding = (0, 10, 10, 0),
-    #         halign = :right)
-    # end
-
     f
 end
 
-function visualize_persistence_and_ma(input, output)
+function visualize_persistence_and_ma_with_diagrams(input, output)
     realizations = [[e for e in eachcol(hcat(MorphoMol.Utilities.get_matrix_realization(state, input["template_centers"])...))] for state in output["states"]]
     realizations = [hcat(realizations[i]...) for i in 1:length(realizations)]
     dgms = output["PDGMs"]
@@ -230,13 +222,142 @@ function visualize_persistence_and_ma(input, output)
 
     scatter!(conf_ax, points, markersize = conf_ms, color = colors, colormap = :rainbow)
 
-    # for (label, layout) in zip(["A", "B", "C", "D"], [cg, pg, mg, eg])
-    #     Label(layout[1, 1, TopLeft()], label,
-    #         fontsize = 12,
-    #         font = :bold,
-    #         padding = (0, 10, 10, 0),
-    #         halign = :right)
-    # end
+    f
+end
+
+function visualize_ma_and_persistence(input, output)
+    realizations = [[e for e in eachcol(hcat(MorphoMol.Utilities.get_matrix_realization(state, input["template_centers"])...))] for state in output["states"]]
+    realizations = [hcat(realizations[i]...) for i in 1:length(realizations)]
+    Es = output["Es"]
+    pf = MorphoMol.Energies.get_prefactors(input["rs"], input["η"])
+    Vs = pf[1] .* output["Vs"]
+    As = pf[2] .* output["As"]
+    Cs = pf[3] .* output["Cs"]
+    Xs = pf[4] .* output["Xs"]
+    OLs = input["overlap_slope"] .* output["OLs"]
+    mol_type = input["mol_type"]    
+    exp_template_centers = MorphoMol.Utilities.TWOTMVSU_EXPERIMENTAL_ASSEMBLY[mol_type]["template_centers"]
+    exp_state = MorphoMol.Utilities.TWOTMVSU_EXPERIMENTAL_ASSEMBLY[mol_type]["state"]
+    thetas = [MorphoMol.Utilities.average_offset_distance(exp_template_centers, input["template_centers"], exp_state, state) for state in output["states"]]
+    xs = [i for i in 1:length(Es)];
+    zero_ps = output["P0s"]
+    one_ps = output["P1s"]
+    two_ps = [0.0 for _ in zero_ps]
+    pw = input["persistence_weights"]
+    try 
+        two_ps = output["P2s"]
+    catch
+        pw = [pw; 1.0]
+    end
+    tps = zero_ps + one_ps + two_ps
+
+    f = Figure(fontsize = 12)
+    ms = 5
+    title_fs = 15
+    cm = :Dark2_8
+    cr = (1,8)
+
+    #Slider
+    sl_i = Slider(f[5, 1:4], range = 1:length(xs), startvalue = 1)
+    x = sl_i.value
+
+
+    # Configuration
+    cg = GridLayout(f[1:2, 1:2])
+    Label(cg[0, 1:2], text = "Configuration", fontsize = title_fs)
+    conf_sc = LScene(cg[1:2, 1:2], show_axis=false)
+    n_atoms = size(realizations[1])[2]
+    n_mol = 2
+    points = @lift([Point3f(realizations[$x][1,i], realizations[$x][2,i], realizations[$x][3,i]) for i in 1:n_atoms])
+    conf_colors = vcat([[j for _ in 1:1206] for j in 1:n_mol]...)
+    conf_ms = 10
+    scatter!(conf_sc, points, markersize = conf_ms, color = conf_colors, colormap = :pastel)
+
+    # Persistence
+    cm = :Spectral_4
+    cr = (1, 4)
+
+    pg = GridLayout(f[1:2, 3:4])
+    Label(pg[0, 1:3], text = "Persistence (not included in Energy)", fontsize = title_fs)
+    zero_p_ax = Axis(pg[2, 1], title = "$(pw[1]) b_0")
+    one_p_ax = Axis(pg[1, 2], title = "$(pw[2]) b_1")
+    two_p_ax = Axis(pg[2, 2], title = "$(pw[3]) b_2")
+    tp_ax = Axis(pg[1, 1], title = L"Σ")
+
+    tp_mark = @lift(Point2f($x, $@lift(tps[$x])))
+
+    zero_p_mark = @lift(Point2f($x, $@lift(zero_ps[$x])))
+    one_p_mark = @lift(Point2f($x, $@lift(one_ps[$x])))
+    two_p_mark = @lift(Point2f($x, $@lift(two_ps[$x])))
+
+    scatter!(zero_p_ax, xs, zero_ps, color = 1, colormap = cm, colorrange = cr, markersize = ms)
+    scatter!(zero_p_ax, zero_p_mark, color=:black, markersize = 10)
+
+    scatter!(one_p_ax, xs, one_ps, color = 2, colormap = cm, colorrange = cr, markersize = ms)
+    scatter!(one_p_ax, one_p_mark, color=:black, markersize = 10)
+
+    scatter!(two_p_ax, xs, two_ps, color = 3, colormap = cm, colorrange = cr, markersize = ms)
+    scatter!(two_p_ax, two_p_mark, color=:black, markersize = 10)
+
+    scatter!(tp_ax, xs, tps, color = 4, colormap = cm, colorrange = cr, markersize = ms)
+    scatter!(tp_ax, tp_mark, color = :black, markersize = 10)
+
+
+
+    #Energy and Theta
+    eg = GridLayout(f[3, 1:4])
+    Label(eg[0, 1:2], text = "Energy", fontsize = title_fs)
+    E_ax = Axis(eg[1:2, 1], title = L"F_{sol}")
+    theta_ax = Axis(eg[1:2, 2], title = L"\Theta")
+
+    theta_mark = @lift(Point2f($x, $@lift(thetas[$x])))
+    E_mark = @lift(Point2f($x, $@lift(Es[$x])))
+
+    scatter!(theta_ax, xs, thetas, color = :magenta, markersize = ms)
+    scatter!(theta_ax, theta_mark, color=:black, markersize = 10)
+
+    scatter!(E_ax, xs, Es, color = :blue, markersize = ms)
+    scatter!(E_ax, E_mark, color=:black, markersize = 10)
+
+    #Measures   
+    mg = GridLayout(f[4, 1:4])
+
+    pf = MorphoMol.Energies.get_prefactors(1.4, 0.3665)
+    Vs = pf[1] .* Vs
+    As = pf[2] .* As
+    Cs = pf[3] .* Cs
+    Xs = pf[4] .* Xs
+
+    Label(mg[0, 1:5], text = "Geometric Measures", fontsize = title_fs)
+    cm = :Set2_5
+    cr = (1, 5)
+
+    Vs_ax = Axis(mg[1, 1], title = L"pV")
+    As_ax = Axis(mg[1, 2], title = L"\sigma A")
+    Cs_ax = Axis(mg[1, 3], title = L"\kappa C")
+    Xs_ax = Axis(mg[1, 4], title = L"\overline{\kappa} X")
+    OLs_ax = Axis(mg[1, 5], title = L"OLs")
+
+    Vs_mark = @lift(Point2f($x, $@lift(Vs[$x])))
+    As_mark = @lift(Point2f($x, $@lift(As[$x])))
+    Cs_mark = @lift(Point2f($x, $@lift(Cs[$x])))
+    Xs_mark = @lift(Point2f($x, $@lift(Xs[$x])))
+    OLs_mark = @lift(Point2f($x, $@lift(OLs[$x])))
+
+    scatter!(Vs_ax, xs, Vs, color = 1, colormap = cm, colorrange = cr, markersize = ms)
+    scatter!(Vs_ax, Vs_mark, color=:black, markersize = 10)
+
+    scatter!(As_ax, xs, As, color = 2, colormap = cm, colorrange = cr, markersize = ms)
+    scatter!(As_ax, As_mark, color=:black, markersize = 10)
+
+    scatter!(Cs_ax, xs, Cs, color = 3, colormap = cm, colorrange = cr, markersize = ms)
+    scatter!(Cs_ax, Cs_mark, color=:black, markersize = 10)
+
+    scatter!(Xs_ax, xs, Xs, color = 4, colormap = cm, colorrange = cr, markersize = ms)
+    scatter!(Xs_ax, Xs_mark, color=:black, markersize = 10)
+
+    scatter!(OLs_ax, xs, OLs, color = 5, colormap = cm, colorrange = cr, markersize = ms)
+    scatter!(OLs_ax, OLs_mark, color=:black, markersize = 10)
 
     f
 end
